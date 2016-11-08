953d4ab11e254618b5f5741d75c9b710ab86355b
Ben Hawkyard <bhawkyard1@gmail.com>
2016-11-08 09:28:10 +0000

    More function pointer stuff.

--- include/cmd.hpp
+++ include/cmd.hpp
@@ -5,17 +5,53 @@
 
 #include "markovChain.hpp"
 
-typedef void (* fpt)(void);
-
+template<class T>
 class cmd
 {
-public:
-    cmd( markovChain * _obj );
-    void reg(const std::string &_name, fpt _pt);
-    void exec(const std::string &_cmd);
+public:	
+		typedef void (T::*fpt)(const std::string&);
+
+		cmd( T * _obj )
+		{
+			m_object = _obj;
+		}
+
+		void reg(const std::string &_name, fpt _pt)
+		{
+			std::pair<std::string, fpt> p(_name, _pt);
+			m_commands.insert(p);
+		}
+
+		void exec(const std::string &_cmd)
+		{
+			(m_obj->m_commands[_cmd])();
+		}
+
 private:
-    markovChain * m_obj;
+		T * m_obj;
     std::unordered_map<std::string, fpt> m_commands;
 };
 
 #endif
+/*
+cmd::cmd(T *_obj)
+{
+		m_obj = _obj;
+}
+
+void cmd::exec(const std::string &_cmd)
+{
+		std::vector<std::string> cmds = split(_cmd, ' ');
+		std::string func = cmds[0];
+		std::vector<std::string> args = cmds;
+		args.erase( args.begin() );
+
+		(m_obj->*m_commands[func])();
+}
+
+void cmd::reg(const std::string &_name, fpt _pt)
+{
+		std::pair<std::string, fpt> p(_name, _pt);
+		m_commands.insert(p);
+}
+*/
--- src/cmd.cpp
+++ src/cmd.cpp
@@ -1,23 +1,2 @@
 #include "cmd.hpp"
 #include "util.hpp"
-
-cmd::cmd(markovChain *_obj)
-{
-    m_obj = _obj;
-}
-
-void cmd::exec(const std::string &_cmd)
-{
-    std::vector<std::string> cmds = split(_cmd, ' ');
-    std::string func = cmds[0];
-    std::vector<std::string> args = cmds;
-    args.erase( args.begin() );
-
-    m_obj->(*m_commands[func])();
-}
-
-void cmd::reg(const std::string &_name, fpt _pt)
-{
-    std::pair<std::string, fpt> p(_name, _pt);
-    m_commands.insert(p);
-}
